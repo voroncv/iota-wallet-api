0840df6af8ff67db8e5e211d2dbda06d2c8e0647
Alexandr Voroncov <defauthack@gmail.com>
2018-07-03 00:56:14 +0300

    Upload API for iota wallet

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const indexRoutes = require('./api/index');
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+	next();
+});
+
+app.use('/', indexRoutes);
+
+app.use(cors());
+
+app.use((req, res, next) => {
+	const error = new Error('Not found');
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+  	error: {
+  		message: error.message
+  	}
+  })
+});
+
+module.exports = app;
